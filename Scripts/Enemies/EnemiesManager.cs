57ca2245baa2a8efdf953b5a0479d82a1d5678f6
miguelmm95 <miguelmartinezmartinez95@gmail.com>
2024-02-03 23:31:56 +0100

    Stage Events

--- Scripts/Enemies/EnemiesManager.cs
+++ Scripts/Enemies/EnemiesManager.cs
@@ -29,7 +29,7 @@ public class EnemiesManager : MonoBehaviour
         }
     }
 
-    private void SpawnEnemy()
+    public void SpawnEnemy()
     {
         Vector3 position = GenerateRandomPosition();
 
