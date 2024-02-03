3b8c77739e6659ae4a5e3329770bed17b0689e4d
miguelmm95 <miguelmartinezmartinez95@gmail.com>
2024-02-03 01:09:22 +0100

    Base Enemy and Player

--- Scripts/Enemies/Enemy.cs
+++ Scripts/Enemies/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform targetDestination;
+    GameObject targetGameObject;
     [SerializeField] float speed;
 
     Rigidbody2D rb2d;
@@ -12,11 +14,25 @@ public class Enemy : MonoBehaviour
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        targetGameObject = targetDestination.gameObject;
     }
 
     private void FixedUpdate()
     {
         Vector3 direction = (targetDestination.position - transform.position).normalized;
         rb2d.velocity = direction * speed;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject == targetGameObject)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        
     }
 }
